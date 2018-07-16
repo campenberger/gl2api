6f102c12a25b1af0925885e0c609f59501f9a6d7
Chris Ampenberger <campenberger@lexington-solutions.com>
2018-07-16 14:37:25 -0400

    Fixed marshmallow to 2.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gl2api',
-      version='0.5',
+      version='0.6',
       description='A basic graylog 2 client API that is incomplete, but avoids the swagger codegen template code',
       url='https://github.com/campenberger/gl2api',
       author='Chris Ampenberger',
@@ -9,5 +9,5 @@ setup(name='gl2api',
       license='MIT',
       packages=['gl2api'],
       zip_safe=False,
-      install_requires=['requests>=2.18', 'marshmallow>=2.15'],
-      setup_requires=['requests>=2.18', 'marshmallow>=2.15'])
+      install_requires=['requests>=2.18', 'marshmallow==2.15.3'],
+      setup_requires=['requests>=2.18', 'marshmallow==2.15.3'])
